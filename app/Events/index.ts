96a636ed2800046ee9cc9e535f91fa8f1597ce33
Osama Khan <Osama6756@gmail.com>
2021-09-21 10:50:13 +0500

    Added object containing possible events list

---
+++ app/Events/index.ts
@@ -0,0 +1,6 @@
+/** An object containing the possible events and their string names */
+export const events = {
+  onConnect: "connection",
+  onSendMessage: "sendMessage",
+  onReceiveMessage: "receiveMessage",
+};
