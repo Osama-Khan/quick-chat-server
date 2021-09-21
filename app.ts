f35e6149471c19ce276226d44a771f0a24eef2bb
Osama Khan <Osama6756@gmail.com>
2021-09-21 10:49:43 +0500

    Added environment setup

---
+++ app.ts
@@ -0,0 +1,10 @@
+import env from "dotenv";
+
+// Set up the environment
+process.env = { ...process.env, ...env.config().parsed };
+
+if (!process.env.PORT || !process.env.CLIENT_URL) {
+  throw new Error(
+    "Please set environment variables in .env file. Rename .env.example to .env for default settings."
+  );
+}
