ca7674f730dcf8f9433b6496ba9f875ee46f005a
Osama Khan <Osama6756@gmail.com>
2021-09-21 10:51:27 +0500

    Added events for connection and messages

--- app/Events/index.ts
+++ app/Events/index.ts
@@ -1,3 +1,7 @@
+import { onConnectEvent } from "./Connection.event";
+import { onSendMessageEvent, emitReceiveMessageEvent } from "./message.event";
+
+export { onConnectEvent, onSendMessageEvent, emitReceiveMessageEvent };
 /** An object containing the possible events and their string names */
 export const events = {
   onConnect: "connection",
