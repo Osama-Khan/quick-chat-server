105bf21b5ef3e046265ec3adffef06706fe16eae
Osama Khan <Osama6756@gmail.com>
2021-09-22 15:33:54 +0500

    Added room event listeners on socket

--- app/Events/connection.event.ts
+++ app/Events/connection.event.ts
@@ -1,5 +1,11 @@
 import { Server, Socket } from "socket.io";
-import { onSendMessageEvent, events } from "./";
+import {
+  onSendMessageEvent,
+  events,
+  onJoinRoomEvent,
+  onCreateRoomEvent,
+  onLeaveRoomEvent,
+} from "./";
 
 /** Event triggered when server receives a connection
  * @param server The server that received the connection
@@ -9,4 +15,13 @@ export function onConnectEvent(server: Server, socket: Socket) {
   socket.on(events.onSendMessage, (message) =>
     onSendMessageEvent(server, socket, message)
   );
+  socket.on(events.onJoinRoom, (id) => {
+    onJoinRoomEvent(socket, id);
+  });
+  socket.on(events.onCreateRoom, () => {
+    onCreateRoomEvent(socket);
+  });
+  socket.on(events.onLeaveRoom, (id) => {
+    onLeaveRoomEvent(socket, id);
+  });
 }
