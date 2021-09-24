037b00ed64fcea886b1b37f05b3674a8fdf8bd11
Osama Khan <Osama6756@gmail.com>
2021-09-24 11:35:47 +0500

    Added connection initialization in createServer method

--- app/index.ts
+++ app/index.ts
@@ -1,11 +1,15 @@
 import { Server } from "socket.io";
 import { events, onConnectEvent } from "./Events";
+import { dbService } from "./services";
 
 /** Creates a QuickChat server
  * @param port Port number to serve at
  * @param clientUrl URL of the client to add in CORS
  */
 export function createServer(port: number, clientUrl: string): void {
+  // Initialize Database connection
+  dbService.initConnection();
+
   const io = new Server(port, {
     cors: {
       origin: [clientUrl],
