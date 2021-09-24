81dec6480f632ec76c46c2c921e5d819c824d6c5
Osama Khan <Osama6756@gmail.com>
2021-09-24 11:38:32 +0500

    Added auth listeners, removed post-auth listeners

--- app/Events/connection.event.ts
+++ app/Events/connection.event.ts
@@ -1,27 +1,17 @@
 import { Server, Socket } from "socket.io";
-import {
-  onSendMessageEvent,
-  events,
-  onJoinRoomEvent,
-  onCreateRoomEvent,
-  onLeaveRoomEvent,
-} from "./";
+import { UserType } from "../Models/user.model";
+import { events, onLoginEvent } from "./";
+import { onRegisterEvent } from "./auth.event";
 
 /** Event triggered when server receives a connection
  * @param server The server that received the connection
  * @param socket The socket of the connection
  */
 export function onConnectEvent(server: Server, socket: Socket) {
-  socket.on(events.onSendMessage, (message, roomId) => {
-    onSendMessageEvent(socket, message, roomId);
-  });
-  socket.on(events.onJoinRoom, (id) => {
-    onJoinRoomEvent(socket, id);
-  });
-  socket.on(events.onCreateRoom, () => {
-    onCreateRoomEvent(socket);
-  });
-  socket.on(events.onLeaveRoom, (id) => {
-    onLeaveRoomEvent(socket, id);
-  });
+  socket.on(events.onLogin, (user: Omit<UserType, "_id">) =>
+    onLoginEvent(socket, user)
+  );
+  socket.on(events.onRegister, (user: Omit<UserType, "_id">) =>
+    onRegisterEvent(socket, user)
+  );
 }
