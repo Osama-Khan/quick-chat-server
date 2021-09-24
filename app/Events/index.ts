0dbcf8487f0a6ec8d6e06f17d0060d97a66ce80d
Osama Khan <Osama6756@gmail.com>
2021-09-24 11:37:38 +0500

    Added Auth events

--- app/Events/index.ts
+++ app/Events/index.ts
@@ -1,4 +1,12 @@
 import { onConnectEvent } from "./Connection.event";
+import {
+  onLoginEvent,
+  emitLoginSuccessEvent,
+  emitLoginFailedEvent,
+  onRegisterEvent,
+  emitRegisterSuccessEvent,
+  emitRegisterFailedEvent,
+} from "./auth.event";
 import { onSendMessageEvent, emitReceiveMessageEvent } from "./message.event";
 import {
   onJoinRoomEvent,
@@ -10,6 +18,12 @@ import {
 
 export {
   onConnectEvent,
+  onLoginEvent,
+  emitLoginSuccessEvent,
+  emitLoginFailedEvent,
+  onRegisterEvent,
+  emitRegisterSuccessEvent,
+  emitRegisterFailedEvent,
   onSendMessageEvent,
   emitReceiveMessageEvent,
   onJoinRoomEvent,
