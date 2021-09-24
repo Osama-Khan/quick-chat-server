2b89ee07035932fc658782fcc0ef9ea0f553b078
Osama Khan <Osama6756@gmail.com>
2021-09-24 11:37:23 +0500

    Updated Message event and DTO for auth based messages

--- app/Events/message.event.ts
+++ app/Events/message.event.ts
@@ -1,18 +1,21 @@
 import { Socket } from "socket.io";
 import { events } from "./";
 import MessageDTO from "../Models/message.dto";
+import { UserType } from "../Models/user.model";
 
 /** Event triggered when client sends a message
  * @param socket The socket of the connection
  * @param message The message sent by the client
  * @param roomId ID of the room the message is sent to
+ * @param user The user that sent the message
  */
 export function onSendMessageEvent(
   socket: Socket,
   message: string,
-  roomId: string
+  roomId: string,
+  user: Partial<UserType>
 ) {
-  const dto = new MessageDTO(message, socket.id, new Date());
+  const dto = new MessageDTO(message, user, new Date());
   emitReceiveMessageEvent(socket, dto, roomId);
 }
 
